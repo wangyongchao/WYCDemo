7c5efb92f1131845f240b8c040dc9f80abc3d68f
yongchao wang <wycxlm506@gmail.com>
2015-10-29 21:18:22 +0800

    Initial commit

---
+++ app/src/main/java/com/weishop/test/custom/CustomActivity.java
@@ -0,0 +1,19 @@
+package com.weishop.test.custom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.weishop.test.R;
+
+public class CustomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}

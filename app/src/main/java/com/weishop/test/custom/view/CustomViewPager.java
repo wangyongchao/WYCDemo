ab23e975e08389d738e34e020d608813fbc56a6a
wangyongchao <wangyongchao@100tal.com>
2018-01-21 18:51:30 +0800

    自定义viewpager

---
+++ app/src/main/java/com/weishop/test/custom/view/CustomViewPager.java
@@ -0,0 +1,42 @@
+package com.weishop.test.custom.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by wangyongchao on 2018/1/21.
+ */
+
+public class CustomViewPager extends ViewGroup {
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+
+}

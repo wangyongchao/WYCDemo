f791a55c4d844a6f09d3a308fee5f5dff5fce605
wangyongchao <wangyongchao237@163.com>
2018-01-24 23:08:31 +0800

    scoller

--- app/src/main/java/com/weishop/test/custom/view/CustomViewPager.java
+++ app/src/main/java/com/weishop/test/custom/view/CustomViewPager.java
@@ -7,7 +7,9 @@ import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 import com.weishop.test.util.TestUtils;
 
@@ -16,21 +18,35 @@ import com.weishop.test.util.TestUtils;
  */
 
 public class CustomViewPager extends ViewGroup {
+    private int mLastX;
+    private int scaledTouchSlop;
+    private boolean isBeingFiling = false;
+    private Scroller scroller;
+
     public CustomViewPager(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CustomViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        scaledTouchSlop = ViewConfiguration.get(this.getContext()).getScaledTouchSlop();
+        scroller = new Scroller(this.getContext());
+
     }
 
     @Override
@@ -67,26 +83,46 @@ public class CustomViewPager extends ViewGroup {
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         int actionMasked = MotionEventCompat.getActionMasked(ev);
-        float x = MotionEventCompat.getX(ev, MotionEventCompat.getActionIndex(ev));
+        int x = (int) MotionEventCompat.getX(ev, MotionEventCompat.getActionIndex(ev));
+        TestUtils.printEvent(ev,"CustomViewPager onInterceptTouchEvent",null);
         switch (actionMasked) {
             case MotionEvent.ACTION_DOWN:
+                mLastX = x;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (Math.abs(mLastX - x) > scaledTouchSlop) {
+                    isBeingFiling = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                isBeingFiling = false;
+                break;
 
+        }
+        return isBeingFiling;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int actionMasked = MotionEventCompat.getActionMasked(event);
+        int x = (int) MotionEventCompat.getX(event, MotionEventCompat.getActionIndex(event));
+        switch (actionMasked) {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = x;
 
                 break;
             case MotionEvent.ACTION_MOVE:
+                int deltX = mLastX - x;
+                scrollBy(deltX, 0);
+
+
                 break;
             case MotionEvent.ACTION_UP:
                 break;
 
         }
 
-
-        return super.onInterceptTouchEvent(ev);
-    }
-
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        return true;
     }
 }

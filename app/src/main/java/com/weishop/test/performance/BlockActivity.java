d1755f65c1d73b4b738ed88439ee282cc5e205bd
wangyongchao <wangyongchao@100tal.com>
2019-03-10 10:18:24 +0800

    fragmetn 权限问题

--- app/src/main/java/com/weishop/test/performance/BlockActivity.java
+++ app/src/main/java/com/weishop/test/performance/BlockActivity.java
@@ -2,6 +2,10 @@
 package com.weishop.test.performance;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,17 +23,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockActivity extends Activity implements View.OnClickListener {
+    private TextView textView;
+    String action = "com.weishop.test.performance.BlockActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_block);
-        findViewById(R.id.block_start).setOnClickListener(this);
+        findViewById(R.id.block_start1).setOnClickListener(this);
+        findViewById(R.id.block_start2).setOnClickListener(this);
+        textView = findViewById(R.id.text);
+        IntentFilter filter = new IntentFilter(action);
+        this.registerReceiver(new MyBroadCastReciver(), filter);
 
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.block_start1) {
+            try {
+                Thread.sleep(10000);
+                System.out.println("fadsfd");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            sendBroadcast(new Intent(action));
 
+        }
+
+
+    }
+
+    class MyBroadCastReciver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                System.out.println("MyBroadCastReciver");
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }

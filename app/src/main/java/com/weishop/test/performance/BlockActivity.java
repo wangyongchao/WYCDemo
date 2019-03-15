f7f57be58e3dcd432c54a56332501a63fbf010a1
wangyongchao <wangyongchao@100tal.com>
2019-03-15 20:31:44 +0800

    block

--- app/src/main/java/com/weishop/test/performance/BlockActivity.java
+++ app/src/main/java/com/weishop/test/performance/BlockActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -21,10 +22,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BlockActivity extends Activity implements View.OnClickListener {
     private TextView textView;
     String action = "com.weishop.test.performance.BlockActivity";
+    private Object lock = new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,20 +51,50 @@ public class BlockActivity extends Activity implements View.OnClickListener {
                 e.printStackTrace();
             }
         } else {
-            sendBroadcast(new Intent(action));
+//            sendBroadcast(new Intent(action));
 
+            new LockTask().execute("");
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (lock) {
+                System.out.println("onClick");
+                // The main thread requires lockedResource here
+                // but it has to wait until LockTask finishes using it.
+            }
         }
 
 
     }
+
+    public class LockTask extends AsyncTask<String, Integer, Long> {
+        @Override
+        protected Long doInBackground(String... params) {
+            synchronized (lock) {
+                // This is a long-running operation, which makes
+                // the lock last for a long time
+                boolean is=true;
+                System.out.println("doInBackground");
+                while (is){
+
+                }
+                return 4l;
+            }
+        }
+    }
+
 
     class MyBroadCastReciver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             try {
-                System.out.println("MyBroadCastReciver");
+                System.out.println("MyBroadCastReciver befor");
                 Thread.sleep(15000);
+                System.out.println("MyBroadCastReciver after");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }

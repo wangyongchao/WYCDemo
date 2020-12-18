cdb8b427b507ff4151a33b6c0f52d0a3a2451e20
wangyongchao <wangyongchao@ttjianbao.com>
2020-12-18 13:16:54 +0800

    constrainlayout

--- app/src/main/java/com/weishop/test/constraint/ConstraintActivity.java
+++ app/src/main/java/com/weishop/test/constraint/ConstraintActivity.java
@@ -2,22 +2,10 @@
 package com.weishop.test.constraint;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.weishop.test.R;
-import com.weishop.test.util.FeedBackWindowManager;
 
 public class ConstraintActivity extends Activity implements View.OnClickListener {
 
@@ -26,7 +14,7 @@ public class ConstraintActivity extends Activity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_constraint);
+        setContentView(R.layout.activity_constraint_sample);
 
     }
 

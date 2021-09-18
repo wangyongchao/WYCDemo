b4e2e72bdfb4da18fc181da4011324568f7c7bb5
wangyongchao <wangyongchao@drojian.dev>
2021-09-18 18:11:49 +0800

    ad test

--- app/src/main/java/com/weishop/test/ads/BannerAdsActivity.kt
+++ app/src/main/java/com/weishop/test/ads/BannerAdsActivity.kt
@@ -2,19 +2,27 @@ package com.weishop.test.ads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import com.google.android.gms.ads.*
 import com.weishop.test.R
 import com.weishop.test.util.LogUtils
+import kotlinx.android.synthetic.main.activity_banner_ads.*
 
-class AdsActivity : AppCompatActivity() {
+/**
+ * 横幅广告 Banner Ads
+ */
+class BannerAdsActivity : AppCompatActivity() {
+    private lateinit var adView: AdView
+    private val needAuto = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ads)
+        setContentView(R.layout.activity_banner_ads)
 
         MobileAds.initialize(this) {
             LogUtils.d("init=${it}")
         }
-        val adView = findViewById<AdView>(R.id.adView)
+        adView = AdView(this)
+        ad_container.addView(adView)
         adView.adListener = object : AdListener() {
 
             /**
@@ -68,8 +76,51 @@ class AdsActivity : AppCompatActivity() {
 
 
         }
+        adView.adUnitId="ca-app-pub-3940256099942544/6300978111"
+        if(needAuto){
+            //自适应广告
+            adView.adSize = adSize
+        }else{
+            adView.adSize=AdSize.BANNER
+        }
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        adView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adView.pause()
+    }
+
+    /**
+     * 退出的时候要销毁
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        adView.destroy()
+    }
+
+    //自适应横幅广告,采用固定宽高比。
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = ad_container.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
 }

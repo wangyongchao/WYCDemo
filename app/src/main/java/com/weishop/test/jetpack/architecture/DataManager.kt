62d0e7d5ae867a5f4c38f5d6e0f1b8cf07ca717f
wangyongchao <wangyongchao@100tal.com>
2021-12-16 22:13:23 +0800

    navigation

---
+++ app/src/main/java/com/weishop/test/jetpack/architecture/DataManager.kt
@@ -0,0 +1,8 @@
+package com.weishop.test.jetpack.architecture
+
+import androidx.lifecycle.MutableLiveData
+
+object DataManager {
+
+    var globalData: MutableLiveData<String> = MutableLiveData()
+}
--- app/src/main/java/com/weishop/test/jetpack/architecture/viewmodellivedata/LiveDataActivity.kt
+++ app/src/main/java/com/weishop/test/jetpack/architecture/viewmodellivedata/LiveDataActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.arch.core.util.Function
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import com.weishop.test.databinding.ActivityViewmodelLivedataBinding
 import com.weishop.test.util.LogUtils
 
@@ -21,23 +24,37 @@ class LiveDataActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
         mBinding.changeData.setOnClickListener {
-            handler.postDelayed(Runnable {
-                mutableLiveData.postValue("post")
-
-            },5000)
-
+            mutableLiveData.postValue("post")
 
         }
 
         mBinding.start.setOnClickListener {
-            mutableLiveData.value="set"
-
+            mutableLiveData.value = "set"
         }
+
 
         mutableLiveData.observe(this) {
             LogUtils.d("observe $it")
 
         }
+
+        val map = Transformations.map(mutableLiveData) {
+            LogUtils.d("observe $it")
+            33
+        }
+        map.observe(this) {
+
+        }
+
+        var transformSwitchMap: LiveData<String> =
+            Transformations.switchMap(mutableLiveData, Function {
+                LogUtils.d("switchMap $it")
+                MutableLiveData<String>(it + "transform")
+            })
+
+        transformSwitchMap.observe(this, Observer {
+            LogUtils.d("transform observe  $it")
+        })
 
 
     }
@@ -45,6 +62,7 @@ class LiveDataActivity : AppCompatActivity() {
     override fun onResume() {
         LogUtils.d("LiveDataActivity onResume")
         super.onResume()
+
     }
 
     override fun onPause() {
